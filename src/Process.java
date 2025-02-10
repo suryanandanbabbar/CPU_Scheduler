34045e99f120b52fbae868381976361084e6fcc2
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-02-10 10:58:55 +0530

    feat: Processes Enable creation of processes - Process Id - Arrival Time - Burst Time

---
+++ src/Process.java
@@ -0,0 +1,9 @@
+public class Process {
+    int id, arrivalTime, burstTime;
+
+    public Process(int id, int arrivalTime, int burstTime) {
+        this.id = id;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+    }
+}
